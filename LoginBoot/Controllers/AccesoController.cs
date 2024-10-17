e9ea59e07d075cf19f0ef98a849867229e0f16f0
Fantino-Russo <fantinojavierrusso@gmail.com>
2024-10-17 09:45:01 -0300

    Agregando nuevo controlador y enviando los datos del login a la clase usuario

---
+++ LoginBoot/Controllers/AccesoController.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+using LoginBoot.Models;
+
+using Sistema_OT.Models;
+
+namespace LoginBoot.Controllers
+{
+    public class AccesoController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Index(string id, string contra)
+        {
+            Usuarios usuario = new Usuarios().encontrarUsuario(id, contra);
+            return View();
+        }
+    }
+}
--- LoginBoot/Controllers/HomeController.cs
+++ LoginBoot/Controllers/HomeController.cs
@@ -13,7 +13,7 @@ namespace LoginBoot.Controllers
             _logger = logger;
         }
 
-        public IActionResult Index()
+        public IActionResult Home()
         {
             return View();
         }
